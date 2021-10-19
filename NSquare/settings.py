c4d8a589c7f0dfe3e0815ea773380dbde7a233e3
Darm Gitkraken <manusporn.darm@gmail.com>
2021-10-20 01:02:39 +0700

    hide secrect key on env ~~~

--- NSquare/settings.py
+++ NSquare/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-11=wh@65ewn1+ue0=65qm9#t(!v00&@g@qi47ek82!vqfu*=f*'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
