36b733c00ebe965b779747d5cd2bcc6c4a441585
Darm Github <darmonlyone@gmail.com>
2021-10-20 20:16:07 +0700

    fix misunderstanding logic max student logic

--- schools/urls.py
+++ schools/urls.py
@@ -2,10 +2,12 @@ from rest_framework_nested import routers
 from django.urls import path, include
 from . import views
 
+# Router for school and students
 router = routers.SimpleRouter()
 router.register('schools', views.SchoolViewSet)
 router.register('students', views.StudentViewSet)
 
+# Nested Router for student look up with school
 student_router = routers.NestedSimpleRouter(router, 'schools', lookup='schools')
 student_router.register('students', views.StudentSchoolViewSet)
 
--- schools/views.py
+++ schools/views.py
@@ -15,19 +15,25 @@ class StudentViewSet(ModelViewSet):
     queryset = Student.objects.all()
 
     def create(self, request, *args, **kwargs):
-        schools = School.objects.all()
-        max_student_of_school = School.objects.get(id=request.data['school'])
+        """
+        Override method create to validate maximum student of the school
+        """
+        if 'school' in request.data:
+            student = Student.objects.filter(school_id=request.data['school'])
+            try:
+                student_school = School.objects.get(id=request.data['school'])
+            except School.DoesNotExist:
+                return Response("School does not exit", status=status.HTTP_400_BAD_REQUEST)
 
-        if schools.count() >= max_student_of_school.max_student:
-            return Response("Maximum number of student reached", status=status.HTTP_409_CONFLICT)
+            if student.count() >= student_school.max_student:
+                return Response("Maximum number of student reached", status=status.HTTP_409_CONFLICT)
 
         serializer = StudentSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(request.data, status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class StudentSchoolViewSet(ModelViewSet):
@@ -46,12 +52,20 @@ class StudentSchoolViewSet(ModelViewSet):
         return Student.objects.filter(school=self.kwargs['schools_pk'])
 
     def create(self, request, *args, **kwargs):
-        schools = School.objects.all()
-        max_student_of_school = School.objects.get(id=kwargs['schools_pk'])
-        request.data['school'] = kwargs['schools_pk']
+        """
+        Override method create to validate maximum student of the school
+        """
+        if 'schools_pk' in kwargs:
+            student = self.get_queryset()
+            try:
+                student_school = School.objects.get(id=kwargs['schools_pk'])
+            except School.DoesNotExist:
+                return Response("School does not exit", status=status.HTTP_400_BAD_REQUEST)
 
-        if schools.count() >= max_student_of_school.max_student:
-            return Response("Maximum number of student reached", status=status.HTTP_409_CONFLICT)
+            if student.count() >= student_school.max_student:
+                return Response("Maximum number of student reached", status=status.HTTP_409_CONFLICT)
+
+            request.data['school'] = student_school.id
 
         serializer = StudentSerializer(data=request.data)
         if serializer.is_valid():
